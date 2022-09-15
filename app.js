0ea41f1f3dba26eb252adeb051dd87e47c17aabd
anshacerbia <l.anshacerbia@gmail.com>
2022-09-15 09:16:45 +0700

    Redeploy

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 const cors = require('cors');
-const routes = require('./routes2');
+const routes = require('./routes');
 
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
