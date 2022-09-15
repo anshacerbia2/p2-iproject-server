cb135396c109246849a904cd371911f18d21c2af
anshacerbia <l.anshacerbia@gmail.com>
2022-09-15 09:13:49 +0700

    Redeploy

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 if (process.env.NODE_ENV !== 'production') require('dotenv').config();
 const express = require('express');
 const app = express();
+const port = process.env.PORT || 3000;
 const cors = require('cors');
-const routes = require('./routes');
+const routes = require('./routes2');
 
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
