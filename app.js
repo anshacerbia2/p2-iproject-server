57e566bdf4368352f4c190c69551523b9c94d31e
anshacerbia <l.anshacerbia@gmail.com>
2022-10-10 16:54:36 +0700

    remove cors

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 3000;
 const cors = require('cors');
 const routes = require('./routes');
 
-app.use(cors());
+// app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use('/', routes);
